44baeffd83e1e8e06a4544eb0ec12794f1cb012d
Max Kofler <kofler.max.dev@gmail.com>
2023-11-20 21:00:30 +0100

    env: Add Environment, EnvironmentExecutable traits
    These new traits provide interfaces for executing in environments

--- src/env.rs
+++ src/env.rs
@@ -1,4 +1,32 @@
 //! Environment structures to represent different environments for actions to take place
 
 mod buildenv;
+use std::{collections::HashMap, ffi::OsString};
+
 pub use buildenv::*;
+
+/// An environment that can execute `EnvironmentExecutables`
+pub trait Environment {
+    /// Executes a `EnvironmentExecutable` in the environment
+    /// # Arguments
+    /// * `executable` - A reference to the executable to execute
+    fn execute(
+        &self,
+        executable: &dyn EnvironmentExecutable,
+    ) -> Result<std::process::ExitStatus, std::io::Error>;
+}
+
+/// An executable that can be executed in a `Environment`
+pub trait EnvironmentExecutable {
+    /// Returns the name of the executable to ease identification
+    fn get_name(&self) -> String;
+
+    /// Returns a hash map of the environment variables to pass to the environment
+    fn get_env_variables(&self) -> HashMap<String, String>;
+
+    /// Returns the command to execute in the environment
+    fn get_command(&self) -> OsString;
+
+    /// Returns the directory to run the command in
+    fn get_workdir(&self) -> OsString;
+}

4da2e2a4828f56484ade676262c09c6f96cd5669
Max Kofler <kofler.max.dev@gmail.com>
2023-11-22 14:38:24 +0000

    Add util::fs::walk_dir_virtual()

--- src/util/fs/walk.rs
+++ src/util/fs/walk.rs
@@ -1,6 +1,6 @@
 use std::path::Path;
 
-/// Walks a directory, calling the callback for every entry found on the way
+/// Walks a directory, calling the callback for every entry found on the way.
 /// # Arguments
 /// * `path` - The path to walk
 /// * `recursive` - If this function should operate recursively
@@ -22,8 +22,48 @@ where
             return Ok(());
         }
 
-        if path.is_dir() && recursive {
+        // Do only walk a subdirectory if it is not a symlink
+        if !path.is_symlink() && path.is_dir() && recursive {
             walk_dir(&path, recursive, callback)?;
+        }
+    }
+
+    Ok(())
+}
+
+/// Walks a directory, calling the callback for every entry found on the way.
+/// This function will also keep track of a virtual path, that does not need to exist in the real world.
+/// # Arguments
+/// * `path` - The path to walk
+/// * `virtual_path` - The virtual path to the directory to walk
+/// * `recursive` - If this function should operate recursively
+/// * `callback` - The callback called for every entry, if it returns `false`, this function will stop
+/// # Errors
+/// Uses the std::fs::read_dir() function which will error on:
+/// - The `path` does not exist
+/// - Permission is denied
+/// - The `path` is not a directory
+pub fn walk_dir_virtual<F>(
+    path: &Path,
+    virtual_path: &Path,
+    recursive: bool,
+    callback: &mut F,
+) -> Result<(), std::io::Error>
+where
+    F: FnMut(std::fs::DirEntry, &Path) -> bool,
+{
+    for entry in std::fs::read_dir(path)? {
+        let entry = entry?;
+        let path = entry.path();
+        let virtual_path = virtual_path.join(path.file_name().unwrap());
+
+        if !callback(entry, &virtual_path) {
+            return Ok(());
+        }
+
+        // Do only walk a subdirectory if it is not a symlink
+        if !path.is_symlink() && path.is_dir() && recursive {
+            walk_dir_virtual(&path, &virtual_path, recursive, callback)?;
         }
     }
 

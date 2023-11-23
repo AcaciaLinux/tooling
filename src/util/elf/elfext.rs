414c938f435f460128aa0d4f9ad96ed7e9afa4f4
Max Kofler <kofler.max.dev@gmail.com>
2023-11-23 12:39:22 +0100

    util::elf::elfext::ELFExt: Add get_runpaths(), fix nulltermination problems
    This commit adds the 'ELFExt::get_runpaths()' function to retrieve the array of runpaths an ELF file has.
    Furthermore this commit fixes an issue where the parsing would not strip 'NULL' terminated strings and leave the null character ('\0') in the string

--- src/util/elf/elfext.rs
+++ src/util/elf/elfext.rs
@@ -2,12 +2,18 @@
 
 use elf::{endian::EndianParse, ElfBytes, ParseError};
 
+static D_TAG_NEEDED: i64 = 1;
+static D_TAG_RPATH: i64 = 15;
+static D_TAG_RUNPATH: i64 = 29;
+
 /// Extended trait function for handling ELF files
 pub trait ELFExt {
     /// Returns the interpreter requested by the ELF binary if one is needed, else `None`
     fn get_interpreter(&self) -> Result<Option<&str>, ParseError>;
     /// Returns the needed shared libraries if available, else `None`
     fn get_shared_needed(&self) -> Result<Option<Vec<&str>>, ParseError>;
+    /// Returns the runpaths split apart if available, else `None`
+    fn get_runpaths(&self) -> Result<Option<Vec<&str>>, ParseError>;
 }
 
 impl<'data, T: EndianParse> ELFExt for ElfBytes<'data, T> {
@@ -18,7 +24,7 @@ impl<'data, T: EndianParse> ELFExt for ElfBytes<'data, T> {
         };
 
         match std::str::from_utf8(self.section_data(&section)?.0) {
-            Ok(s) => Ok(Some(s)),
+            Ok(s) => Ok(Some(s.trim_end_matches(0 as char))),
             Err(_) => Ok(None),
         }
     }
@@ -39,14 +45,41 @@ impl<'data, T: EndianParse> ELFExt for ElfBytes<'data, T> {
         let mut res: Vec<&str> = Vec::new();
 
         for sym in section_dyn {
-            if sym.d_tag != 1 {
-                continue;
+            if sym.d_tag == D_TAG_NEEDED {
+                match dynsyms.get(sym.d_val() as usize) {
+                    Err(_) => continue,
+                    Ok(s) => res.push(s.trim_end_matches(0 as char)),
+                };
             }
+        }
 
-            match dynsyms.get(sym.d_val() as usize) {
-                Err(_) => continue,
-                Ok(s) => res.push(s),
-            };
+        Ok(Some(res))
+    }
+
+    fn get_runpaths(&self) -> Result<Option<Vec<&str>>, ParseError> {
+        let common = self.find_common_data()?;
+
+        let dynsyms = match common.dynsyms_strs {
+            None => return Ok(None),
+            Some(s) => s,
+        };
+
+        let section_dyn = match self.dynamic()? {
+            None => return Ok(None),
+            Some(s) => s,
+        };
+
+        let mut res: Vec<&str> = Vec::new();
+
+        for sym in section_dyn {
+            if sym.d_tag == D_TAG_RUNPATH || sym.d_tag == D_TAG_RPATH {
+                match dynsyms.get(sym.d_val() as usize) {
+                    Err(_) => continue,
+                    Ok(s) => s
+                        .split(':')
+                        .for_each(|p| res.push(p.trim_end_matches(0 as char))),
+                };
+            }
         }
 
         Ok(Some(res))

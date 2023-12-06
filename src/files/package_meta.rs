7e2fdaaa153d1b7451be1a0d8cc1892cc370c60e
Max Kofler <kofler.max.dev@gmail.com>
2023-12-06 16:46:57 +0100

    fixup! Add files::package_meta

--- src/files/package_meta.rs
+++ src/files/package_meta.rs
@@ -10,7 +10,6 @@ pub struct PackageFile {
     pub version: u32,
 
     /// The package this file describes
-    #[serde(deserialize_with = "deserialize_package")]
     pub package: Package,
 }
 
@@ -23,57 +22,12 @@ pub struct Package {
     pub real_version: u32,
     pub description: String,
 
-    pub dependencies: Vec<Dependency>,
+    pub dependencies: HashMap<String, Dependency>,
 }
 
 /// A dependency of the package in the package metadata file
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct Dependency {
-    pub name: String,
     pub req_version: String,
-    pub lnk_version: String,
-}
-
-/// Deserializes a `PackageMeta` struct from a deserializer
-fn deserialize_package<'de, D>(deserializer: D) -> Result<Package, D::Error>
-where
-    D: serde::Deserializer<'de>,
-{
-    #[derive(Debug, Clone, Deserialize, Serialize)]
-    pub struct PackageRaw {
-        pub name: String,
-        pub version: String,
-        pub arch: String,
-        pub real_version: u32,
-        pub description: String,
-
-        pub dependencies: HashMap<String, DependencyRaw>,
-    }
-
-    #[derive(Debug, Clone, Deserialize, Serialize)]
-    pub struct DependencyRaw {
-        pub req_version: String,
-        pub lnk_version: String,
-    }
-
-    let package = PackageRaw::deserialize(deserializer)?;
-
-    let dependencies: Vec<Dependency> = package
-        .dependencies
-        .into_iter()
-        .map(|m| Dependency {
-            name: m.0.to_owned(),
-            req_version: m.1.req_version,
-            lnk_version: m.1.lnk_version,
-        })
-        .collect();
-
-    Ok(Package {
-        name: package.name,
-        version: package.version,
-        arch: package.arch,
-        real_version: package.real_version,
-        description: package.description,
-        dependencies,
-    })
+    pub lnk_version: Option<String>,
 }

c88cfa76495a3deba229ded64038a6fe3a8f70c2
Max Kofler <kofler.max.dev@gmail.com>
2023-12-09 10:31:07 +0000

    lib: Add DIST_DIR constant

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 //! This crate provides tooling for the [AcaciaLinux](https://github.com/AcaciaLinux) project
 
+/// The DIST directory. Default is `acacia`. This **HAS** to be relative to be able to join onto other paths
+pub static DIST_DIR: &str = "acacia";
+
 /// The commit hash of the commit this binary was built from
 const GIT_COMMIT_HASH: &str = env!("GIT_COMMIT_HASH");
 

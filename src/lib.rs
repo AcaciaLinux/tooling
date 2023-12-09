0ba6f7dc1c6dd8ff4b4175f40849e52b8119ae01
Max Kofler <kofler.max.dev@gmail.com>
2023-12-09 16:16:11 +0000

    DIST_DIR: dist_dir() and abs_dist_dir()

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,19 @@
 //! This crate provides tooling for the [AcaciaLinux](https://github.com/AcaciaLinux) project
 
+use std::path::PathBuf;
+
 /// The DIST directory. Default is `acacia`. This **HAS** to be relative to be able to join onto other paths
 pub static DIST_DIR: &str = "acacia";
+
+/// Provide a relative `PathBuf` pointing to the `DIST_DIR`
+pub fn dist_dir() -> PathBuf {
+    PathBuf::from(DIST_DIR)
+}
+
+/// Provide an absolute `PathBuf` pointing to the `DIST_DIR` relative to `/`
+pub fn abs_dist_dir() -> PathBuf {
+    PathBuf::from("/").join(dist_dir())
+}
 
 /// The commit hash of the commit this binary was built from
 const GIT_COMMIT_HASH: &str = env!("GIT_COMMIT_HASH");

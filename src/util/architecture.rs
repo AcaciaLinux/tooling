77653eb3f8615ade25b1bdc5d4e707300b020337
Max Kofler <kofler.max.dev@gmail.com>
2024-03-22 21:39:57 +0100

    util::architecture: Add factory functions

--- src/util/architecture.rs
+++ src/util/architecture.rs
@@ -1,7 +1,12 @@
 //! Utilities for working with architectures
 
+use crate::error::Error;
+use serde::{Deserialize, Serialize};
+
+use crate::error::ErrorExt;
+
 /// An architecture description containing a main architecture and subarchitectures
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Deserialize, Serialize)]
 pub struct Architecture {
     /// The main architecture
     pub arch: String,
@@ -24,6 +29,31 @@ pub fn is_subset<T: PartialEq>(subset: &[T], of: &[T]) -> bool {
 }
 
 impl Architecture {
+    /// Creates a new `Architecture` from the two arguments
+    /// # Arguments
+    /// * `arch` - The main architecture
+    /// * `subarchs` - The sub-architectures
+    pub fn new(arch: String, subarchs: Vec<String>) -> Self {
+        Self { arch, subarchs }
+    }
+
+    /// Creates a new `Architecture` from only the main architecture and no subarchs
+    /// # Arguments
+    /// * `arch` - The main architecture
+    pub fn new_arch(arch: String) -> Self {
+        Self {
+            arch,
+            subarchs: Vec::new(),
+        }
+    }
+
+    /// Creates a new architecture by detecting it using the `uname` crete
+    pub fn new_uname() -> Result<Self, Error> {
+        let info = uname::uname().e_context(|| "Determining host architecture".to_string())?;
+
+        Ok(Self::new(info.machine, Vec::new()))
+    }
+
     /// Checks if this architecture can run on `on`.
     ///
     /// This will check if `self` is a subset of `on`

95c1322a0b5b1af400015a44d21eddf3c30c10b6
Max Kofler <kofler.max.dev@gmail.com>
2024-05-14 21:09:21 +0200

    Add hash::hash_stream()

--- src/util/hash.rs
+++ src/util/hash.rs
@@ -1,5 +1,9 @@
 //! Utilities for dealing with hashes
-use std::{fs::File, io::copy, path::Path};
+use std::{
+    fs::File,
+    io::{copy, Read},
+    path::Path,
+};
 
 use sha2::{digest::Output, Digest, Sha256};
 
@@ -23,6 +27,17 @@ pub fn hash_file(file: &Path) -> Result<Output<Sha256>, Error> {
     let mut f = File::open(file).e_context(context)?;
     let mut hasher = Sha256::new();
     copy(&mut f, &mut hasher).e_context(context)?;
+
+    Ok(hasher.finalize())
+}
+
+/// Hashes a readable stream
+/// # Arguments
+/// * `input` - The stream to hash
+pub fn hash_stream<R: Read>(input: &mut R) -> Result<Output<Sha256>, Error> {
+    let mut hasher = Sha256::new();
+
+    copy(input, &mut hasher).e_context(|| "Hashing stream")?;
 
     Ok(hasher.finalize())
 }

16f12d71a0aceab449597e9494d8617274f71029
Max Kofler <kofler.max.dev@gmail.com>
2024-12-08 20:25:11 +0100

    ODBDriver: Add ObjectTemplateStream
    This allows for normal and prehashed streams

--- src/model/object/objectdb/driver.rs
+++ src/model/object/objectdb/driver.rs
@@ -1,3 +1,5 @@
+use std::io::Read;
+
 use crate::{
     error::Error,
     model::{Object, ObjectCompression, ObjectID, ObjectReader, ObjectType, SeekRead},
@@ -32,23 +34,64 @@ pub trait ODBDriver {
     fn retrieve(&self, oid: &ObjectID) -> Result<Option<ObjectReader>, Error>;
 }
 
+/// A stream that provides the data of the object to
+/// the object template consumer
+pub enum ObjectTemplateStream<'a> {
+    /// The data is passed normally, the object id is computed
+    /// by seeking the stream and hashing the data
+    Normal(&'a mut dyn SeekRead),
+    /// Data is already prehashed and there is no need to
+    /// seek around in the stream
+    Prehashed {
+        /// The stream providing the data
+        stream: &'a mut dyn Read,
+        /// The object ID that results from hashing the stream
+        oid: ObjectID,
+    },
+}
+
 /// A template to create an object of by inserting it into an object database driver
 pub struct ObjectTemplate<'a> {
-    stream: &'a mut dyn SeekRead,
+    stream: ObjectTemplateStream<'a>,
     ty: ObjectType,
     dependencies: Vec<ObjectID>,
 }
 
 impl<'a> ObjectTemplate<'a> {
-    /// Create a new object database driver
+    /// Create a new object template
+    /// # Arguments
     /// * `stream` - The stream to use as the object data
     /// * `ty` - The type of object at hand
     /// * `dependencies` - The dependencies of the object
     pub fn new(stream: &'a mut dyn SeekRead, ty: ObjectType, dependencies: Vec<ObjectID>) -> Self {
         Self {
-            stream,
+            stream: ObjectTemplateStream::Normal(stream),
             ty,
             dependencies,
         }
+    }
+
+    /// Create a new object template from a prehashed stream
+    /// # Arguments
+    /// * `stream` - The stream to store
+    /// * `oid` - The prehashed object id of the stream
+    /// * `ty` - The object type at hand
+    /// * `dependencies` - The dependencies of the object
+    pub fn new_prehashed(
+        stream: &'a mut dyn Read,
+        oid: ObjectID,
+        ty: ObjectType,
+        dependencies: Vec<ObjectID>,
+    ) -> Self {
+        Self {
+            stream: ObjectTemplateStream::Prehashed { stream, oid },
+            ty,
+            dependencies,
+        }
+    }
+
+    /// Splits the template up into its stream, type and dependencies
+    pub fn split_up(self) -> (ObjectTemplateStream<'a>, ObjectType, Vec<ObjectID>) {
+        (self.stream, self.ty, self.dependencies)
     }
 }

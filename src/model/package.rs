91608550cacffc5b6b429b033dff3a4f232295d7
Max Kofler <max@acacialinux.org>
2025-01-29 18:08:20 +0100

    Package: Add insert(), json() and toml() methods

--- src/model/package.rs
+++ src/model/package.rs
@@ -1,8 +1,11 @@
+use std::io::Cursor;
+
+use log::debug;
 use serde::{Deserialize, Serialize};
 
-use crate::util::architecture::Architecture;
+use crate::{error::Error, model::ObjectType, util::architecture::Architecture};
 
-use super::ObjectID;
+use super::{Object, ObjectCompression, ObjectDB, ObjectID};
 
 /// A package in the AcaciaLinux ecosystem
 #[derive(Deserialize, Serialize, Debug)]
@@ -30,4 +33,47 @@ pub struct Package {
     /// This tells a consumer if the package had additional
     /// lower directories when it was built
     pub tainted: bool,
+}
+
+impl Package {
+    /// Returns the `TOML` string for this package
+    pub fn toml(&self) -> String {
+        toml::to_string_pretty(self).expect("Serialize package file should never fail")
+    }
+
+    /// Returns the `JSON` string for this package
+    pub fn json(&self) -> String {
+        serde_json::to_string(self).expect("Serialize package file should never fail")
+    }
+
+    /// Inserts this package into `object_db`
+    /// # Arguments
+    /// * `object_db` - The objet db to insert the package into
+    /// * `compression` - The compression to apply for inserting
+    pub fn insert(
+        &self,
+        object_db: &mut ObjectDB,
+        compression: ObjectCompression,
+    ) -> Result<Object, Error> {
+        let mut cursor = Cursor::new(self.json());
+
+        let mut dependencies = vec![self.tree.clone()];
+        for d in &self.dependencies {
+            dependencies.push(d.clone());
+        }
+
+        let object = object_db.insert_stream(
+            &mut cursor,
+            ObjectType::AcaciaFormula,
+            compression,
+            dependencies,
+        )?;
+
+        debug!(
+            "Inserted package {}@{} as {}",
+            self.name, self.version, object.oid
+        );
+
+        Ok(object)
+    }
 }

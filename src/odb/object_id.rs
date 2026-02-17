c8fb1f92b4b7c5272555dad4b9959f2b3736c59b
Max Kofler <kofler.max.dev@gmail.com>
2026-02-17 21:41:58 +0100

    ObjectID: Add from sha256_{file;stream}()

--- src/odb/object_id.rs
+++ src/odb/object_id.rs
@@ -1,4 +1,14 @@
-use std::{fmt::Display, path::PathBuf};
+use std::{
+    fmt::Display,
+    io::Read,
+    path::{Path, PathBuf},
+};
+
+use crate::{
+    error::{ALErrorExt, ALResult},
+    str,
+    util::hash::{sha256_file, sha256_stream},
+};
 
 /// The SHA256 object id is identified by a ASCII 'S', 0x53
 const OID_ID_SHA256: u8 = 0x53;
@@ -14,6 +24,32 @@ enum InnerObjectID {
 }
 
 impl ObjectID {
+    /// Derives a SHA256 Object ID from the file at
+    /// the provided path by reading and hashing
+    /// all of its contents
+    /// # Arguments
+    /// * `file` - The file to be hashed to an object id
+    pub fn from_sha256_file(file: &Path) -> ALResult<Self> {
+        let ctx = str!("Deriving Object ID from stream");
+
+        Ok(sha256_file(file).ctx(ctx)?.into())
+    }
+
+    /// Derives a SHA256 Object ID from the provided
+    /// stream by reading from it and hashing the contents
+    ///
+    /// This function does not respect the offset the stream
+    /// is currently at, so if this is a file, the position
+    /// is ignored and hashing simply starts from the current
+    /// point in the stream
+    /// # Arguments
+    /// * `stream` - The stream to has into an object id
+    pub fn from_sha256_stream<R: Read>(stream: &mut R) -> ALResult<Self> {
+        let ctx = str!("Deriving Object ID from stream");
+
+        Ok(sha256_stream(stream).ctx(ctx)?.into())
+    }
+
     /// Returns the ID (the digest) as a vector of binary data
     pub fn id(&self) -> Vec<u8> {
         match self.inner {

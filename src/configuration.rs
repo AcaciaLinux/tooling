4773e57dfc95b477c6fe5d085866ea5ef38e3ee2
Max Kofler <kofler.max.dev@gmail.com>
2026-03-01 22:14:21 +0100

    util::hash: hash_stream: Allow pass-through of data
    This allows the hash_stream functions to both, hash the data and pass it on for furhter processing.

--- src/configuration.rs
+++ src/configuration.rs
@@ -6,3 +6,6 @@ pub const SUFFIX_ACACIA_OBJECT: &str = "aobj";
 
 /// The suffix (or file extension) for a raw Acacia object
 pub const SUFFIX_RAW_OBJECT: &str = "robj";
+
+/// The size of the buffer used when hashing a stream or a file in the [util::hash](crate::util::hash) module.
+pub(crate) const HASH_COPY_BUFFER_SIZE: usize = 4096;
--- src/util/hash/mod.rs
+++ src/util/hash/mod.rs
@@ -1,12 +1,18 @@
 //! Utilities for hashing things
-use std::{fs::File, io::Read, path::Path};
+use std::{
+    fs::File,
+    io::{Read, Write},
+    path::Path,
+};
 
 use sha2::{Digest, Sha256, digest::Output};
 
 use crate::{
+    configuration::HASH_COPY_BUFFER_SIZE,
     error::{ALErrorExt, ALResult},
     fs::PathExt,
     str,
+    util::io::NullSink,
 };
 
 /// SHA256 Hashes the supplied string
@@ -21,19 +27,38 @@ pub fn sha256_string(string: &str) -> Output<Sha256> {
 /// SHA256 Hashes a file
 /// # Arguments
 /// * `file` - The path to the file to hash
-pub fn sha256_file(file: &Path) -> ALResult<Output<Sha256>> {
+/// The amount of data hashed (in bytes) and the resulting hash digest
+pub fn sha256_file(file: &Path) -> ALResult<(usize, Output<Sha256>)> {
     let ctx = str!("SHA256 Hashing {}", file.str_lossy());
     let mut f = File::open(file).ctx(ctx)?;
-    sha256_stream(&mut f).ctx(ctx)
+    sha256_stream(&mut f, &mut NullSink::default()).ctx(ctx)
 }
 
 /// SHA256 Hashes a readable stream
 /// # Arguments
 /// * `input` - The stream to hash
-pub fn sha256_stream<R: Read>(input: &mut R) -> ALResult<Output<Sha256>> {
+/// * `output` - The stream to pass the hashed data to after it has been hashed
+/// # Returns
+/// The amount of data hashed (in bytes) and the resulting hash digest
+pub fn sha256_stream<R: Read, W: Write>(
+    input: &mut R,
+    output: &mut W,
+) -> ALResult<(usize, Output<Sha256>)> {
+    let ctx = str!("SHA256 hashing stream");
+
     let mut hasher = Sha256::new();
+    let mut buf = [0u8; HASH_COPY_BUFFER_SIZE];
+    let mut total_len = 0;
 
-    std::io::copy(input, &mut hasher).ctx(str!("SHA256 Hashing stream"))?;
+    loop {
+        let len = input.read(&mut buf).ctx(ctx)?;
+        if len == 0 {
+            break;
+        }
+        hasher.write(&buf[..len]).ctx(ctx)?;
+        output.write(&buf[..len]).ctx(ctx)?;
+        total_len += len;
+    }
 
-    Ok(hasher.finalize())
+    Ok((total_len, hasher.finalize()))
 }
